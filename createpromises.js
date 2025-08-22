2a927e51f2bedbcc5001f9dcb6e4e4e1ef106a2c
Rohit-kr07 <rohitkrmodi07@gmail.com>
2025-08-22 15:44:17 +0530

    Add files via upload

---
+++ createpromises.js
@@ -0,0 +1,15 @@
+function fetchDataWithDelay(data, ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!data) {
+                reject(new Error('No data provided!'));
+            } else {
+                resolve(`Data: ${data}`); 
+            }
+        }, ms);
+    });
+}
+
+fetchDataWithDelay('Hello', 5000)
+    .then(result => console.log(result))
+    .catch(err => console.log('Error:', err.message)); 
